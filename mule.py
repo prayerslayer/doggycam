316b17f5d9b777df773878534d67ae1ad7101e0d
Nikolaus Piccolotto <hello@npiccolotto.com>
2021-04-24 11:28:51 +0200

    Try this

--- mule.py
+++ mule.py
@@ -47,7 +47,7 @@ def main():
 
             cmd = task["command"]
 
-            if cmd == "preview":
+            if cmd == "preview" and device_state == DeviceState.Ready:
                 # use video port for perfect positioning of camera
                 device.capture(
                     "./static/preview.jpg",
