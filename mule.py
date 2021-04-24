2d82d4b122c625ac600223a56605892cfd3263ed
Nikolaus Piccolotto <hello@npiccolotto.com>
2021-04-24 15:28:49 +0200

    Try thready

--- mule.py
+++ mule.py
@@ -7,6 +7,8 @@ import subprocess
 import os
 from time import sleep
 import glob
+from threading import Thread
+from queue import Queue
 import toml
 from enum import Enum
 
@@ -20,74 +22,84 @@ class DeviceState(Enum):
     Ready = 1
     Recording = 2
 
+queue = Queue(maxsize=5)
+
+def thready():
+    device_state = DeviceState.Initializing
+    device = PiCamera(
+        resolution=(config["camera"]["width"], config["camera"]["height"]),
+        framerate=config["camera"]["fps"],
+    )
+    device.annotate_frame_num = True
+    device.annotate_background = Color("black")
+    device.annotate_text = datetime.now().strftime("%Y-%m-%d %H:%M")
+    device_state = DeviceState.Ready
+    sleep(2)
+
+    while True:
+        task = queue.get()
+        cmd = task['command']
+
+        if cmd == "preview":
+            # use video port for perfect positioning of camera
+            device.capture(
+                "./static/preview.jpg",
+                use_video_port=True,
+            )
+
+        elif cmd == "update_ts":
+            device.annotate_text = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+        elif cmd == "start_rec":
+            filename = task["filename"]
+            try:
+                device.start_recording(
+                    f"{filename}.h264",
+                    format="h264",
+                    quality=config["camera"]["quality"],
+                )
+                device_state = DeviceState.Recording
+            except PiCameraException.PiCameraAlreadyRecording:
+                print("Cannot start recording, recording already in progress.")
+
+        elif cmd == "stop_rec":
+            try:
+                device.stop_recording()
+                device_state = DeviceState.Ready
+                for filename in glob.glob("./static/*.h264"):
+                    abs_filename = os.path.abspath(filename)
+                    if os.path.exists(f"{abs_filename}.mp4"):
+                        subprocess.run(["rm", f"{abs_filename}.mp4"])
+                    subprocess.run(
+                        ["MP4Box", "-add", abs_filename, f"{abs_filename}.mp4"]
+                    )
+                    subprocess.run(["MP4Box", "-inter", "500", f"{abs_filename}.mp4"])
+                    if not config["debug"]:
+                        subprocess.run(["rm", abs_filename])
+            except PiCameraException.PiCameraNotRecording:
+                print("Cannot stop recording, no recording is in progress.")
+
+        elif cmd == 'shutdown':
+            device.close()
+            queue.task_done()
+            return
+
+        queue.task_done()
 
 def main():
-    try:
-        device_state = DeviceState.Initializing
-        device = PiCamera(
-            resolution=(config["camera"]["width"], config["camera"]["height"]),
-            framerate=config["camera"]["fps"],
-        )
-        device.annotate_frame_num = True
-        device.annotate_background = Color("black")
-        device.annotate_text = datetime.now().strftime("%Y-%m-%d %H:%M")
-        device_state = DeviceState.Ready
+    Thread(target=thready, daemon=True).start()
+    while True:
+        print(f"Mule waiting, device is {device_state}...")
+        message = uwsgi.mule_get_msg()
+        print(f"Mule received: {message}")
 
-        while True:
-            print(f"Mule waiting, device is {device_state}...")
-            message = uwsgi.mule_get_msg()
-            print(f"Mule received: {message}")
+        if message == "":
+            print("SHUTDOWN")
+            queue.put({ 'command': 'shutdown'})
+            return
 
-            if message == "":
-                print("SHUTDOWN")
-                device.close()
-                return
-
-            task = json.loads(message)
-
-            cmd = task["command"]
-
-            if cmd == "preview":
-                # use video port for perfect positioning of camera
-                device.capture(
-                    "./static/preview.jpg",
-                    use_video_port=True,
-                )
-
-            elif cmd == "update_ts":
-                device.annotate_text = datetime.now().strftime("%Y-%m-%d %H:%M")
-
-            elif cmd == "start_rec":
-                filename = task["filename"]
-                try:
-                    device.start_recording(
-                        f"{filename}.h264",
-                        format="h264",
-                        quality=config["camera"]["quality"],
-                    )
-                    device_state = DeviceState.Recording
-                except PiCameraException.PiCameraAlreadyRecording:
-                    print("Cannot start recording, recording already in progress.")
-
-            elif cmd == "stop_rec":
-                try:
-                    device.stop_recording()
-                    device_state = DeviceState.Ready
-                    for filename in glob.glob("./static/*.h264"):
-                        abs_filename = os.path.abspath(filename)
-                        if os.path.exists(f"{abs_filename}.mp4"):
-                            subprocess.run(["rm", f"{abs_filename}.mp4"])
-                        subprocess.run(
-                            ["MP4Box", "-add", abs_filename, f"{abs_filename}.mp4"]
-                        )
-                        subprocess.run(["MP4Box", "-inter", "500", f"{abs_filename}.mp4"])
-                        if not config["debug"]:
-                            subprocess.run(["rm", abs_filename])
-                except PiCameraException.PiCameraNotRecording:
-                    print("Cannot stop recording, no recording is in progress.")
-
-    finally:
-        device.close()
+        task = json.loads(message)
+        queue.put(task)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,25 +1,38 @@
 from picamera import PiCamera, Color, exc as PiCameraException
 from time import sleep
 import signal
+import sys
+from threading import Thread
 
-device = PiCamera(
-            resolution=(1640, 922,
-            30,
-        )
-device.annotate_frame_num = True
-filename = 'test'
-
-def handler(_,_):
+def handler(_1,_2):
     device.stop_recording()
     device.close()
+    sys.exit(0)
 
 signal.signal(signal.SIGINT, handler)
 
-
-device.start_recording(f"{filename}.h264",
+def loop1():
+    device = PiCamera(
+                resolution=(1640, 922),
+                framerate=30,
+            )
+    device.annotate_frame_num = True
+    sleeptime = 5
+    filename = f'test_sleep{sleeptime}'
+    device.start_recording(f"{filename}.h264",
                         format="h264",
                         quality=30)
+    
 
-while True:
-    sleep(1)
-    print('recording)
+
+
+def loop2():
+    print("Hi! I am loop2.")
+    while True:
+        sleep(1)
+        print("zzz")
+
+t = Thread(target=loop1)
+t.daemon = True
+t.start()
+loop2()
